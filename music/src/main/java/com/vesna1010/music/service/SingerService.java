d0c550bb818c3d3bfbb6aa96114a0c0d94e0edc3
Vesna S <vesnasdoboj@gmail.com>
2019-02-19 02:11:33 +0100

    Update SingerService.java

--- music/src/main/java/com/vesna1010/music/service/SingerService.java
+++ music/src/main/java/com/vesna1010/music/service/SingerService.java
@@ -3,11 +3,12 @@ package com.vesna1010.music.service;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import com.vesna1010.music.model.Singer;
 
 public interface SingerService {
 
-	List<Singer> findAll();
+	List<Singer> findAll(Sort sort);
 
 	Page<Singer> findAll(Pageable pageable);
 
@@ -18,3 +19,4 @@ public interface SingerService {
 	void deleteById(Long id);
 
 }
+
