76589bd77dab366f410e831ba717de6172681969
Vesna S <vesnasdoboj@gmail.com>
2019-02-19 02:10:47 +0100

    Update AlbumService.java

--- music/src/main/java/com/vesna1010/music/service/AlbumService.java
+++ music/src/main/java/com/vesna1010/music/service/AlbumService.java
@@ -3,11 +3,12 @@ package com.vesna1010.music.service;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import com.vesna1010.music.model.Album;
 
 public interface AlbumService {
 
-	List<Album> findAll();
+	List<Album> findAll(Sort sort);
 
 	Page<Album> findAll(Pageable pageable);
 		
