2f0807e492bd52e1c4db6ae282ad55881e194692
Vesna <vesnasdoboj@gmail.com>
2019-08-22 06:15:55 +0200

    Update Song.java

--- music/src/main/java/com/vesna1010/music/model/Song.java
+++ music/src/main/java/com/vesna1010/music/model/Song.java
@@ -89,5 +89,30 @@ public class Song {
 	public void setCounter(Long counter) {
 		this.counter = counter;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Song other = (Song) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
