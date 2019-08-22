04e02141f0374edeb161914421da7c60967e213d
Vesna <vesnasdoboj@gmail.com>
2019-08-22 06:05:13 +0200

    Update SingerValidator.java

--- music/src/main/java/com/vesna1010/music/validator/SingerValidator.java
+++ music/src/main/java/com/vesna1010/music/validator/SingerValidator.java
@@ -1,6 +1,7 @@
 package com.vesna1010.music.validator;
 
 import java.time.LocalDate;
+
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -9,7 +10,7 @@ import com.vesna1010.music.model.Singer;
 @Component
 public class SingerValidator implements Validator {
 
-	private String nameRegex = "^[a-zA-Z\\s]{5,}$";
+	public static final String NAME_REGEXP = "^[a-zA-Z\\s]{5,}$";
 
 	@Override
 	public boolean supports(Class<?> cls) {
@@ -19,22 +20,31 @@ public class SingerValidator implements Validator {
 	@Override
 	public void validate(Object object, Errors errors) {
 		Singer singer = (Singer) object;
-		String name = singer.getName();
-		LocalDate birthDate = singer.getBirthDate();
-		byte[] photo = singer.getPhoto();
 
-		if (!name.matches(nameRegex)) {
+		if (isInvalidName(singer.getName())) {
 			errors.rejectValue("name", "singer.name");
 		}
 
-		if (birthDate == null) {
-			errors.rejectValue("birthDate", "singer.birthDate");
-		}
-
-		if (photo == null || photo.length == 0) {
+		if (isInvalidPhoto(singer.getPhoto())) {
 			errors.rejectValue("photo", "singer.photo");
 		}
 
+		if (isInvalidBirthDate(singer.getBirthDate())) {
+			errors.rejectValue("birthDate", "singer.birthDate");
+		}
+
+	}
+
+	private boolean isInvalidName(String name) {
+		return (name == null || !name.matches(NAME_REGEXP));
+	}
+
+	private boolean isInvalidPhoto(byte[] photo) {
+		return (photo == null || photo.length == 0);
+	}
+
+	private boolean isInvalidBirthDate(LocalDate birthDate) {
+		return (birthDate == null);
 	}
 
 }
