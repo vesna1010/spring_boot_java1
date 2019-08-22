1984b0a6fddce6d4329899682cfdb3cb7e36b0ed
Vesna <vesnasdoboj@gmail.com>
2019-08-22 06:06:10 +0200

    Update SongValidator.java

--- music/src/main/java/com/vesna1010/music/validator/SongValidator.java
+++ music/src/main/java/com/vesna1010/music/validator/SongValidator.java
@@ -3,14 +3,13 @@ package com.vesna1010.music.validator;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
-
 import com.vesna1010.music.model.Album;
 import com.vesna1010.music.model.Song;
 
 @Component
 public class SongValidator implements Validator {
 
-	private String titleRegex = "^[a-zA-Z0-9\\s]{3,}$";
+	public static final String TITLE_REGEXP = "^[a-zA-Z0-9\\s]{3,}$";
 
 	@Override
 	public boolean supports(Class<?> cls) {
@@ -20,22 +19,31 @@ public class SongValidator implements Validator {
 	@Override
 	public void validate(Object object, Errors errors) {
 		Song song = (Song) object;
-		String title = song.getTitle();
-		Album album = song.getAlbum();
-		byte[] file = song.getFile();
 
-		if (!title.matches(titleRegex)) {
+		if (isInvalidTitle(song.getTitle())) {
 			errors.rejectValue("title", "song.title");
 		}
 
-		if (album == null) {
+		if (isInvalidAlbum(song.getAlbum())) {
 			errors.rejectValue("album", "song.album");
 		}
 
-		if (file == null || file.length == 0) {
+		if (isInvalidFile(song.getFile())) {
 			errors.rejectValue("file", "song.file");
 		}
 
+	}
+
+	private boolean isInvalidTitle(String title) {
+		return (title == null || !title.matches(TITLE_REGEXP));
+	}
+
+	private boolean isInvalidAlbum(Album album) {
+		return (album == null);
+	}
+
+	private boolean isInvalidFile(byte[] file) {
+		return (file == null || file.length == 0);
 	}
 
 }
