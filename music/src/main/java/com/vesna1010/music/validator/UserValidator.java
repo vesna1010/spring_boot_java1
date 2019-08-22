e80478d4662ffd7189e73df290676ae01e675c5d
Vesna <vesnasdoboj@gmail.com>
2019-08-22 06:07:43 +0200

    Update UserValidator.java

--- music/src/main/java/com/vesna1010/music/validator/UserValidator.java
+++ music/src/main/java/com/vesna1010/music/validator/UserValidator.java
@@ -10,11 +10,12 @@ import com.vesna1010.music.service.UserService;
 @Component
 public class UserValidator implements Validator {
 
+	public static final String NAME_REGEXP = "^[a-zA-Z\\s]{5,}$";
+	public static final String EMAIL_REGEXP = "^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
+	public static final String PASSWORD_REGEXP = "^\\S{8,15}$";
+
 	@Autowired
 	private UserService service;
-	private String nameRegex = "^[a-zA-Z\\s]{5,}$";
-	private String emailPattern = "^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
-	private String passwordPattern = "^\\S{8,15}$";
 
 	@Override
 	public boolean supports(Class<?> cls) {
@@ -24,31 +25,43 @@ public class UserValidator implements Validator {
 	@Override
 	public void validate(Object object, Errors errors) {
 		User user = (User) object;
-		String name = user.getName();
-		String email = user.getEmail();
-		String password = user.getPassword();
-		String confirmPassword = user.getConfirmPassword();
 
-		if (!name.matches(nameRegex)) {
+		if (isInvalidName(user.getName())) {
 			errors.rejectValue("name", "user.name");
 		}
 
-		if (!email.matches(emailPattern)) {
+		if (isInvalidEmail(user.getEmail())) {
 			errors.rejectValue("email", "user.email.pattern");
 		}
 
-		if (service.existsByEmail(email)) {
+		if (service.existsByEmail(user.getEmail())) {
 			errors.rejectValue("email", "user.email.exists");
 		}
 
-		if (!password.matches(passwordPattern)) {
+		if (isInvalidPassword(user.getPassword())) {
 			errors.rejectValue("password", "user.password");
 		} else {
-			if (!password.equals(confirmPassword)) {
+			if (isDifferentPasswords(user.getPassword(), user.getConfirmPassword())) {
 				errors.rejectValue("confirmPassword", "user.passwordConfirmDifferent");
 			}
 		}
 
+	}
+
+	private boolean isInvalidName(String name) {
+		return (name == null || !name.matches(NAME_REGEXP));
+	}
+
+	private boolean isInvalidEmail(String email) {
+		return (email == null || !email.matches(EMAIL_REGEXP));
+	}
+
+	private boolean isInvalidPassword(String password) {
+		return (password == null || !password.matches(PASSWORD_REGEXP));
+	}
+
+	public boolean isDifferentPasswords(String password, String confirmPassword) {
+		return !password.equals(confirmPassword);
 	}
 
 }
